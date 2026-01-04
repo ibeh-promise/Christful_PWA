11d91736eecb33ee636d00b8c8dcb60255e559f5
danimpulse500 <danimpulse500@gmail.com>
2026-01-04 14:10:12 +0100

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 import Image from "next/image";
+import { LoginLayout } from "@/components/auth/Login";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white font-sans dark:bg-black">
-      <Image
-        src="/logo.png"
-        alt="Christful Logo"
-        width={300}
-        height={300}
-        className="animate-fade-in"
-      />
-    </div>
+    <LoginLayout />
   );
 }
